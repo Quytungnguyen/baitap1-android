97cf13d933c2c0e7608692592def5b23bc72c1c5
Quytungnguyen <Tungbeotn888@gmail.com>
2025-03-02 01:30:59 +0700

    lỗi

---
+++ app/src/main/java/com/example/baitap1/HinhVuong.java
@@ -0,0 +1,20 @@
+package com.example.baitap1;
+
+public class HinhVuong {
+    private int a;
+
+    public int chuvi(){
+        return 4*a;
+    }
+    public int dientich(){
+        return a*a;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
--- app/src/main/java/com/example/baitap1/MainActivity.java
+++ app/src/main/java/com/example/baitap1/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.baitap1;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +15,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView chuvi,dientich;
+    EditText a;
+    Button tinh,chuyen;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +28,28 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        chuvi = findViewById(R.id.chuvi);
+        dientich = findViewById(R.id.dientich);
+        a = findViewById(R.id.a);
+        tinh = findViewById(R.id.tinh);
+        chuyen = findViewById(R.id.chuyen);
+
+        tinh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HinhVuong hinhVuong = new HinhVuong();
+                hinhVuong.setA(Integer.parseInt(a.getText().toString()));
+                chuvi.setText("Chu vi: " + hinhVuong.chuvi());
+                dientich.setText("Diện tích:" + hinhVuong.dientich());
+            }
+        });
+        chuyen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(getApplicationContext(), MainActivity2.class);
+                startActivity(intent);
+            }
         });
     }
 }
